902c747f5c619725ab99dd43b305758ea208e427
KhaledTarek <kingdodo987@gmail.com>
2022-02-20 21:47:29 +0200

    fixed allowed hosts

--- corey_blog/settings.py
+++ corey_blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = ['https://khaledblogapp.herokuapp.com/']
+ALLOWED_HOSTS = ['khaledblogapp.herokuapp.com']
 
 
 # Application definition
